fe2de992c2b6c16e12f9159da47d7bed6325f081
jaymarystella <jaymarystella@gmail.com>
2023-11-19 00:13:12 +0900

    [BS-2] Meta클래스에 JsonProperty 적용

--- bs-external/kakao-client/src/main/java/com/bs/kakao/Meta.java
+++ bs-external/kakao-client/src/main/java/com/bs/kakao/Meta.java
@@ -1,12 +1,18 @@
 package com.bs.kakao;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.ToString;
 
 @Getter
 @ToString
 public class Meta {
-    private int total_count;
-    private int pageable_count;
-    private boolean is_end;
+    @JsonProperty("total_count")
+    private int totalCount;
+
+    @JsonProperty("pageable_count")
+    private int pageableCount;
+
+    @JsonProperty("is_end")
+    private boolean end;
 }
