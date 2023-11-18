4acecad70034c6cc2a6cd295558106ca6f2778fd
jaymarystella <jaymarystella@gmail.com>
2023-11-18 15:56:44 +0900

    initial commit

---
+++ src/main/java/com/bs/bsapi/BsApiApplication.java
@@ -0,0 +1,13 @@
+package com.bs.bsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bs/bsapi/BsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs.bsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
