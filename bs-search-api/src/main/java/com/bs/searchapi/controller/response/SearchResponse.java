38a244585fa8240808cc6fcd374b2a2b10b1cc2f
jaymarystella <jaymarystella@gmail.com>
2023-11-19 17:49:33 +0900

    [BS-3] 블로그 검색 API Swagger 설정

--- bs-search-api/src/main/java/com/bs/searchapi/controller/response/SearchResponse.java
+++ bs-search-api/src/main/java/com/bs/searchapi/controller/response/SearchResponse.java
@@ -1,17 +1,31 @@
 package com.bs.searchapi.controller.response;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@ApiModel(value = "블로그 검색 결과", description = "검색 결과를 나타내는 모델")
 public class SearchResponse {
+    @ApiModelProperty(value = "블로그 제목", example = "치킨 먹고 싶다")
     private final String title;
+
+    @ApiModelProperty(value = "블로그 글 요약", example = "요새 유행은 양념치킨...")
     private final String summary;
+
+    @ApiModelProperty(value = "블로그 글 URL", example = "https://developers.kakao.com")
     private final String url;
+
+    @ApiModelProperty(value = "블로그 이름", example = "치킨마스터")
     private final String blogName;
+
+    @ApiModelProperty(value = "블로그 글 대표 이미지", example = "https://developers.kakao.com/assets/img/about/logos/kakaolink/kakaolink_btn_small.png")
     private final String thumbnail;
+
+    @ApiModelProperty(value = "블로그 발생 시각", example = "2023-11-09T01:46:00")
     private final LocalDateTime issuedAt;
 
     @Builder
