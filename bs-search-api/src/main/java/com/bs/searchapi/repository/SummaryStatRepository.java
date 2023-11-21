0e8d37f9632258a6cec23e504893df61b4bfe4b2
jaymarystella <jaymarystella@gmail.com>
2023-11-22 08:08:04 +0900

    [BS-0] 불필요한 코드 정리
    - 일간 파티셔닝 이후 뒷단에서 만 summary와 머지하여 통계정보 제공하려고했으나 h2에서의 파티셔닝 미지원 및 일정이슈로 현재버전으로 마무리
    - key값들 별도 관리
    - db validate 옵션 추가

--- bs-search-api/src/main/java/com/bs/searchapi/repository/SummaryStatRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bs.searchapi.repository;
-
-import com.bs.searchapi.entity.SummaryStat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SummaryStatRepository extends JpaRepository<SummaryStat, String> {
-}
---
+++ bs-search-api/src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE daily_stat
+(
+    id              BIGINT AUTO_INCREMENT PRIMARY KEY,
+    keyword         VARCHAR(50),
+    event_date_time DATETIME
+);
